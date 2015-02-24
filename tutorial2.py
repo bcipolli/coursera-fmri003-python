63788faacd98f301f95e987e48aafb1a58916abb
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-24 01:01:41 -0800

    Add GLM regression and visualization.

--- tutorial2.py
+++ tutorial2.py
@@ -13,9 +13,16 @@ import numpy as np
 import os
 
 import nibabel
+from nilearn.input_data import NiftiMasker
 from nipy.modalities.fmri import design_matrix
 from nipy.modalities.fmri.experimental_paradigm import BlockParadigm
+from nipy.modalities.fmri.glm import FMRILinearModel
+from nipy.labs.viz import plot_map, cm
 
+
+########################################
+# Load the data
+########################################
 
 # Input the functional volume
 print("Loading all functional images into a single volume...")
@@ -27,8 +34,13 @@ func_img = nibabel.funcs.concat_images(all_files)
 # Images start with 004, so slice is unnecessary.
 # func_img = index_img(func_img, slice(4))  # eliminate images 1-4
 
+
+########################################
 # Create the design matrix
-# Used http://cogmaster-stats.github.io/python-cogstats/auto_examples/plot_localizer_analysis.html
+########################################
+# From http://cogmaster-stats.github.io/python-cogstats/auto_examples/plot_localizer_analysis.html
+# and https://github.com/nipy/nipy/blob/master/examples/labs/demo_dmtx.py
+
 n_scans = 96
 n_blocks = 16
 n_events = 12
@@ -58,14 +70,65 @@ def parse_motion(motion_file):
 motion_data = parse_motion(motion_file)
 motion_params = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz']
 
-X1 = design_matrix.make_dmtx(frametimes, paradigm=paradigm,
-                             hrf_model='canonical',
-                             drift_model='cosine', drift_order=2,  # quadratic
-                             add_regs=motion_data, add_reg_names=motion_params)
+dmat = design_matrix.make_dmtx(frametimes, paradigm=paradigm,
+                               hrf_model='canonical',
+                               drift_model='cosine', drift_order=2,  # quadratic
+                               add_regs=motion_data, add_reg_names=motion_params)
 
-# plot the results
+# plot the design matrix
 fig = plt.figure(figsize=(10, 6))
 ax = fig.gca()
-X1.show(ax=ax)
+dmat.show(ax=ax)
 ax.set_title('Event-related design matrix', fontsize=12)
+plt.show()
+
+########################################
+# Perform a GLM analysis
+########################################
+# From https://github.com/nipy/nipy/blob/master/examples/labs/need_data/glm_beta_and_variance.py
+print('Fitting a GLM (this takes time)...')
+fmri_glm = FMRILinearModel(func_img, dmat.matrix,
+                           mask='compute')
+fmri_glm.fit(do_scaling=True, model='ar1')
+
+########################################
+# Output beta and variance images
+########################################
+beta_hat = fmri_glm.glms[0].get_beta()  # Least-squares estimates of the beta
+variance_hat = fmri_glm.glms[0].get_mse()  # Estimates of the variance
+mask = fmri_glm.mask.get_data() > 0
+
+# output beta images
+dim = dmat.matrix.shape[1]
+beta_map = np.tile(mask.astype(np.float)[..., np.newaxis], dim)
+beta_map[mask] = beta_hat.T
+beta_image = nibabel.Nifti1Image(beta_map, fmri_glm.affine)
+beta_image.get_header()['descrip'] = (
+    'Parameter estimates of the localizer dataset')
+output_file = 'beta.nii'
+nibabel.save(beta_image, output_file)
+print("Beta image witten in %s" % output_file)
+
+variance_map = mask.astype(np.float)
+variance_map[mask] = variance_hat
+
+# Create a snapshots of the variance image contrasts
+vmax = np.log(variance_hat.max())
+plot_map(func_img.get_data()[..., 0], func_img.affine, title='Original data (t=0)')
+plot_map(np.log(variance_map + .1),
+         fmri_glm.affine,
+         cmap=cm.hot_black_bone,
+         vmin=np.log(0.1),
+         vmax=vmax,
+         anat=None,
+         threshold=.1, alpha=.9,
+         title='Estimates of the variance.')
+
+# Show beta map for each beta
+fh = plt.figure()
+mask = NiftiMasker()
+mask.fit(func_img)
+for pi in range(len(dmat.names)):
+    ax = fh.add_subplot(3, 4, pi + 1)
+    plot_map(beta_map[..., pi], fmri_glm.affine, title=dmat.names[pi], axes=ax)
 plt.show()
